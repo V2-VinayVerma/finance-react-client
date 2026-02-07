43fc37081e971ba7623b1939e1a252bc17973372
V2-VinayVerma <vermastar0987654321@gmail.com>
2026-02-07 12:22:54 +0530

    app Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,152 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import AppLayout from "./components/AppLayout";
+import { useEffect, useState } from "react";
+import Dashboard from "./pages/Dashboard";
+import Logout from "./pages/Logout";
+import UserLayout from "./components/UserLayout";
+import axios from "axios";
+import { serverEndpoint } from "./config/appConfig";
+import { useSelector, useDispatch } from "react-redux";
+import { SET_USER } from "./redux/user/action";
+import Groups from "./pages/Groups";
+import GroupExpenses from "./pages/GroupExpenses";
+import ManageUsers from "./pages/ManageUsers";
+import UnauthorizedAccess from "./components/errors/UnauthorizedAccess";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const dispatch = useDispatch();
+    // Value of userDetails represents whether the user
+    // is logged in or not.
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // useSelector takes in 1 function as input. Redux calls the function that
+    // you pass to useSelector with all the values its storing/managing.
+    // We need to take out userDetails since we're interested in userDetails object.
+    const userDetails = useSelector((state) => state.userDetails);
+    const [loading, setLoading] = useState(true);
+
+    const isUserLoggedIn = async () => {
+        try {
+            const response = await axios.post(
+                `${serverEndpoint}/auth/is-user-logged-in`,
+                {},
+                { withCredentials: true }
+            );
+
+            // setUserDetails(response.data.user);
+            dispatch({
+                type: SET_USER,
+                payload: response.data.user,
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        isUserLoggedIn();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="container text-center">
+                <h3>Loading...</h3>
+            </div>
+        );
+    }
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    userDetails ? (
+                        <Navigate to="/dashboard" />
+                    ) : (
+                        <AppLayout>
+                            <Home />
+                        </AppLayout>
+                    )
+                }
+            />
+            <Route
+                path="/login"
+                element={
+                    userDetails ? (
+                        <Navigate to="/dashboard" />
+                    ) : (
+                        <AppLayout>
+                            <Login />
+                        </AppLayout>
+                    )
+                }
+            />
+
+            <Route
+                path="/dashboard"
+                element={
+                    userDetails ? (
+                        <UserLayout>
+                            <Groups />
+                        </UserLayout>
+                    ) : (
+                        <Navigate to="/login" />
+                    )
+                }
+            />
+
+            <Route
+                path="/groups/:groupId"
+                element={
+                    userDetails ? (
+                        <UserLayout>
+                            <GroupExpenses />
+                        </UserLayout>
+                    ) : (
+                        <Navigate to="/login" />
+                    )
+                }
+            />
+
+            <Route
+                path="/manage-users"
+                element={
+                    userDetails ? (
+                        <ProtectedRoute roles={["admin"]}>
+                            <UserLayout>
+                                <ManageUsers />
+                            </UserLayout>
+                        </ProtectedRoute>
+                    ) : (
+                        <Navigate to="/login" />
+                    )
+                }
+            />
+
+            <Route
+                path="/unauthorized-access"
+                element={
+                    userDetails ? (
+                        <UserLayout>
+                            <UnauthorizedAccess />
+                        </UserLayout>
+                    ) : (
+                        <AppLayout>
+                            <UnauthorizedAccess />
+                        </AppLayout>
+                    )
+                }
+            />
+
+            <Route
+                path="/logout"
+                element={userDetails ? <Logout /> : <Navigate to="/login" />}
+            />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
