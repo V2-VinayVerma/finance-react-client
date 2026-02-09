e3c8a177b3dfcefbb1d8ad07cc678d868fbac6eb
V2-VinayVerma <vermastar0987654321@gmail.com>
2026-02-09 22:51:59 +0530

    users frontend

---
+++ src/components/errors/UnauthorizedAcceess.jsx
@@ -0,0 +1,14 @@
+function UnauthorizedAccess() {
+    return (
+        <div className="conatiner p-5">
+            <h2>Unauthorized Access</h2>
+            <p className="text-muted">
+                you do not have permission to view this page.
+                Contact your Admin for further Assistance!
+                Thank You !!
+            </p>
+        </div>
+    )
+}
+
+export default UnauthorizedAccess;
