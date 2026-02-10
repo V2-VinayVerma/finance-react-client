64c92156bd495145987465081e79d924f5ec76ad
V2-VinayVerma <vermastar0987654321@gmail.com>
2026-02-10 10:06:03 +0530

    rbac

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Groups from "./pages/Groups";
 import GroupExpenses from "./pages/GroupExpenses";
 import ManageUsers from "./pages/ManageUsers";
 import UnauthorizedAccess from "./components/errors/UnauthorizedAccess";
+import ProtectedRoute from "./rbac/ProtectedRoute";
 
 function App() {
     const dispatch = useDispatch();
--- src/rbac/ProtectedRoute.jsx
+++ src/rbac/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-function protectedRoutes({ roles, children }) {
+function ProtectedRoute({ roles, children }) {
     const user = useSelector(state => state.userDetails);
     return roles.includes(user?.role) ?
         children :
-        <Navigate to='/unauthorixed-access' />
+        <Navigate to='/unauthorized-access' />
 }
 
-export default protectedRoutes;
+export default ProtectedRoute;
